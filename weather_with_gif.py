247b0bfbb6bc82e2d9c0a579933466ee1375aef6
Itay-Jacobson <itay.jacobson@post.runi.ac.il>
2023-03-22 19:45:10 +0200

    Changed conditions, giphy_url method, and optimized giphy_params in weather_with_gif.py

--- weather_with_gif.py
+++ weather_with_gif.py
@@ -18,17 +18,19 @@ def weather(city):
     open_weather_response = requests.get(
         url=open_weather_url, params=open_weather_params)
     temp = open_weather_response.json()['main']['temp']
-    giphy_url = "https://api.giphy.com/v1/gifs/search"
+    giphy_url = "https://api.giphy.com/v1/gifs/translate"
+    giphy_params = {}
+    search_word = ""
     if temp > 25:
-        giphy_params = {'api_key': giphy_token, 'q': 'spicy', 'limit': 1}
-    elif temp < 25 and temp > 15:
-        giphy_params = {'api_key': giphy_token,
-                        'q': 'i love it', 'limit': 1}
+        search_word = 'spicy weather'
+    elif temp > 15:
+        search_word = 'i love it'
     else:
-        giphy_params = {'api_key': giphy_token,
-                        'q': 'stay home', 'limit': 1}
+        search_word = 'stay home'
+    giphy_params = {'api_key': giphy_token,
+                    's': search_word}
     giphy_response = requests.get(url=giphy_url, params=giphy_params)
-    gif = giphy_response.json()['data'][0]['images']['original']['url']
+    gif = giphy_response.json()['data']['images']['original']['url']
     return f'Hello {city}! it is {str(temp)} <img src={gif}>'
 
 
